b19abf70e1f6e925e976304aa147169ca09a6ee0
Aaron Ye <yea@sfusd.edu>
2017-08-02 15:42:46 -0700

    Changed HTMl for site

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+def post_list(request):
+    # tales request amd returns a function render that will render (put together)
+    # our template blog/post_list.html
+    return render(request, 'blog/post_list.html', {})
